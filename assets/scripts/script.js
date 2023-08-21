23c4613efa791ec38f7b18d2e46cf6fd9ca6e929
ewi josepha <ewijosepha@gmail.com>
2023-08-21 19:12:33 +0200

    printing numbers to screen

---
+++ assets/scripts/script.js
@@ -0,0 +1,25 @@
+
+// const display= document.getElementById("display")
+// const check= document.getElementById("check")
+
+// let arr=[]
+
+// const numm = () => {
+//   const startt= document.getElementById("start")
+//   const endd= document.getElementById("end")
+//   console.log(endd)
+//   for(i=startt.value; i<=endd.value; i++){
+//     arr.push(i)
+//   }
+//   console.log(arr)
+//   display.innerHTML = `this is the result ${arr}`
+// }
+// check.addEventListener("click", numm);
+
+
+
+
+
+ 
+
+
