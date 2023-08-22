c85085e4d8d5e362231e52d362c2d2ea3ec154d2
ewi josepha <ewijosepha@gmail.com>
2023-08-22 16:04:19 +0200

    fixing lintes issues

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,16 @@
-const display = document.getElementById('display');
-const check = document.getElementById('check');
+const display = document.getElementById('display')
+const check = document.getElementById('check')
 
-const arr = [];
+const arr = []
 
 const numm = (item) => {
-  const startt = document.getElementById('start');
-  const endd = document.getElementById('end');
-  console.log(endd);
-  for (i = startt.value; i <= endd.value; i++) {
-    arr.push(i);
+  const startt = document.getElementById('start')
+  const endd = document.getElementById('end')
+  console.log(endd)
+  for (let i = startt.value; i <= endd.value; i++) {
+    arr.push(i)
   }
-  console.log(arr);
-  display.innerHTML = `this is the result ${arr}`;
-};
+  console.log(arr)
+  display.innerHTML = `this is the result ${arr}`
+}
 check.addEventListener('click', numm)
